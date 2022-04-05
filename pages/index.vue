66b58eab41489a573651dc8ce64e488c27b8b8f3
Patryk Kurzeja <patryk.kurzeja@monterail.com>
2022-04-05 23:09:31 +0200

    mess

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <script setup>
 const { data } = await useFetch(() =>'https://rickandmortyapi.com/api/character?page=1')
 
-const results = computed(() => data.value.results)
 </script>
 
 <template>
     <div>
       <div
-        v-for="character in results"
+        v-for="character in data.results"
         :key="character.id"
       >
        <h3>
