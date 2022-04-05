3f0782f66ae08e98116998a8492d4b436d6739fa
Patryk Kurzeja <patryk.kurzeja@monterail.com>
2022-04-05 21:20:33 +0200

    change nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ["~/assets/css/tailwind.css"],
-  target: "static",
   build: {
     postcss: {
       postcssOptions: {
