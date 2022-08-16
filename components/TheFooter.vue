2f6b8d73d29997a43aa029f6e1a4ecf40db80da2
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2022-08-16 20:20:32 +0200

    fix logo in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,14 +3,15 @@
     <div class="container max-w-6xl px-4 mx-auto">
       <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="FlowBite Logo">
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+              <a href="https://flowbite.com/" class="flex items-center text-white">
+
+                  <img src="@/assets/images/bk-awards-full-logo.svg" class="text-white mr-3 h-24" alt="FlowBite Logo">
+                  <!-- <span class="self-center text-2xl font-bold whitespace-nowrap dark:text-white">Flowbite</span> -->
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Resources</h2>
+                  <p class="mb-6 text-lg font-bold text-white uppercase dark:text-white">Resources</p>
                   <ul class="text-white dark:text-gray-400">
                       <li class="mb-4">
                           <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
@@ -21,7 +22,7 @@
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Follow us</h2>
+                  <p class="mb-6 text-lg font-bold text-white uppercase dark:text-white">Follow us</p>
                   <ul class="text-white dark:text-gray-400">
                       <li class="mb-4">
                           <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
@@ -32,13 +33,13 @@
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Legal</h2>
+                  <p class="mb-6 text-lg font-bold text-white uppercase dark:text-white">Legal</p>
                   <ul class="text-white dark:text-gray-400">
                       <li class="mb-4">
-                          <a href="privacy" class="hover:underline">Privacy Policy</a>
+                          <nuxt-link to="privacy" class="hover:underline">Privacy Policy</nuxt-link>
                       </li>
                       <li>
-                          <a href="terms-and-conditions" class="hover:underline">Terms &amp; Conditions</a>
+                          <nuxt-link to="terms-and-conditions" class="hover:underline">Terms &amp; Conditions</nuxt-link>
                       </li>
                   </ul>
               </div>
@@ -46,11 +47,11 @@
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
       <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span class="text-lg text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
           </span>
           <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
               <a href="#" class="text-gray-500 hover:text-white dark:hover:text-white">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path></svg>
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12p.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12p.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path></svg>
                   <span class="sr-only">Facebook page</span>
               </a>
               <a href="#" class="text-gray-500 hover:text-white dark:hover:text-white">
