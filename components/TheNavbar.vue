e14d1c9079ebe989327cb409d2ba71b0f4c2457d
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2022-09-16 14:07:22 +0200

    Mobile nav

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,4 +1,3 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <header class="fixed z-20 w-full bg-white">
     <div class="container mx-auto max-w-screen-xl px-4 sm:px-6">
@@ -15,14 +14,58 @@
             />
           </a>
         </div>
-        <!-- hamburger button -->
-        <div class="ml-auto flex">
-          <div class="-my-2 -mr-2 md:hidden">
+
+        <nav
+          :class="[
+            'sidenav fixed right-0 bottom-0 top-0 z-20 px-6 md:hidden',
+            { open },
+          ]"
+        >
+          <div class="flex py-6">
+            <button
+              class="ml-auto inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              @click="open = false"
+            >
+              <span class="sr-only">Close menu</span>
+              <nuxt-icon
+                name="x"
+                fill
+                class="flex h-5 w-5 items-center justify-center"
+              />
+              <MenuIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <ul :class="{ flex: open, hidden: !open }" class="flex-col">
+            <li v-for="link in links" :key="link.name">
+              <nuxt-link
+                class="flex my-3 rounded-md px-6 py-3 text-base font-medium hover:bg-new-gray hover:text-dark-golden"
+                :to="link.href"
+                >{{ link.name }}</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link
+                to="register"
+                class="flex my-3 items-center justify-center rounded-md bg-royal-blue px-10 py-3 text-center text-base font-medium text-white hover:bg-yale-blue"
+              >
+                Register
+              </nuxt-link>
+            </li>
+          </ul>
+        </nav>
+        <!-- desktop menu -->
+        <div class="flex ml-auto">
+          <div class="md:hidden">
             <button
               class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              @click="open = true"
             >
               <span class="sr-only">Open menu</span>
-              <!-- <MenuIcon class="h-6 w-6" aria-hidden="true" /> -->
+              <nuxt-icon
+                name="menu"
+                fill
+                class="flex h-5 w-5 items-center justify-center"
+              />
             </button>
           </div>
           <div class="hidden space-x-2 md:flex">
@@ -30,7 +73,7 @@
               v-for="link in links"
               :key="link.name"
               :to="link.href"
-              class="rounded-md px-6 py-3 text-base font-medium hover:bg-new-gray hover:text-dark-golden"
+              class="rounded-md px-5 py-2 text-base font-medium hover:bg-new-gray hover:text-dark-golden"
             >
               {{ link.name }}
             </nuxt-link>
@@ -38,7 +81,7 @@
           <div class="hidden items-center md:flex">
             <nuxt-link
               to="register"
-              class="ml-6 inline-flex items-center justify-center rounded-md bg-royal-blue px-10 py-3 text-center text-base font-medium text-white hover:bg-yale-blue"
+              class="ml-5 inline-flex items-center justify-center rounded-md bg-royal-blue px-5 py-2 text-center text-base font-medium text-white hover:bg-yale-blue"
             >
               Register
             </nuxt-link>
@@ -72,4 +115,33 @@ const links = [
     href: 'tickets',
   },
 ]
+const open = ref(true)
 </script>
+
+<style scoped>
+.sidenav {
+  @apply flex-col bg-white;
+
+  transform: translateX(100%);
+  transition: transform 600ms cubic-bezier(0.23, 1, 0.32, 1) 0s;
+  min-width: 100%;
+
+  /* width: 100%; */
+
+  /* @screen sm {
+    min-width: 24rem;
+  } */
+}
+
+.sidenav.open {
+  transform: translateX(0);
+}
+
+.sidenav-menu {
+  @apply flex-grow px-4 pb-4;
+
+  /* @screen md {
+    @apply block overflow-y-auto pb-0;
+  } */
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
+  <div>
     <TheNavbar />
     <!-- <TheNavbarTemplate /> -->
-      <!-- <header class="container px-4 py-8 mx-auto">
+    <!-- <header class="container px-4 py-8 mx-auto">
         <NuxtLink to="/">| HOME</NuxtLink>
         <NuxtLink to="/characters">| CHARACTERS</NuxtLink>
         <NuxtLink to="/posts">| POSTS</NuxtLink>
         <NuxtLink to="/mountains">| MOUNTAINS</NuxtLink>
       </header> -->
-      <main class="pt-24">
-        <slot/>
-      </main>
-      <TheFooter />
-    </div>
+    <main class="pt-24">
+      <slot />
+    </main>
+    <TheFooter />
+  </div>
 </template>
