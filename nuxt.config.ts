a3db8ad46ea12f1a93139b42844cbb719c7e934e
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2022-11-05 19:51:24 +0100

    fix rc13 and add hybrid generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,19 +3,19 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css', '@/assets/css/main.css'],
-  modules: ['nuxt-icons'],
+  modules: ['nuxt-icons', '@nuxtjs/tailwindcss'],
   plugins: ['@/plugins/vue-select.ts'],
   router: {
-    trailingSlash: false,
+    // trailingSlash: false,
   },
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
+  routeRules: {
+    // Static page generated on-demand, revalidates in background
+    '/categories': { swr: true },
+    // Static page generated on-demand once
+    '/': { static: true },
+    '/register': { static: true },
+    // Add redirect headers
+    '/old-page': { redirect: '/categories' },
+    '/old-page2': { redirect: { to: '/new-page', statusCode: 302 } },
   },
 })
