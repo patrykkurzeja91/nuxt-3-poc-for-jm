b7dbd89fd92f1530b53058f901c9dfcd587be55f
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2023-01-09 20:12:30 +0100

    remove unnecessary dep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@
 export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css', '@/assets/css/main.css'],
   modules: ['nuxt-icons', '@nuxtjs/tailwindcss'],
-  plugins: ['@/plugins/vue-select.ts'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
