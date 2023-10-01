30000ac6949f4dbb33a79c83625189322983d025
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2023-10-01 21:12:55 +0200

    add font faces form googlestatic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,10 +40,10 @@ export default defineNuxtConfig({
           href: 'https://fonts.gstatic.com',
           crossorigin: 'anonymous',
         },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Playfair+Display:wght@400;500&display=swap',
-        },
+        // {
+        //   rel: 'stylesheet',
+        //   href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Playfair+Display:wght@400;500&display=swap',
+        // },
       ],
       meta: [
         {
